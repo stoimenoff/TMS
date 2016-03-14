bf0464ccb1aa2d3774d55f97345904eb8c21e20e
stoimenoff <stoimenoffjr@gmail.com>
2016-03-14 16:09:15 +0200

    Free email check on registration added

--- src/main/java/registration/Registrator.java
+++ src/main/java/registration/Registrator.java
@@ -19,25 +19,25 @@ public final class Registrator {
 	public static void register(User user)
 			throws UsernameAlreadyInUseException, EmailAlreadyInUseException {
 		MongoClient mongoClient = new MongoClient("localhost", 27017);
+		try {
+			MongoDatabase db = mongoClient.getDatabase("tmsDB");
+			MongoCollection<Document> usersColl = db.getCollection("users");
 
-		MongoDatabase db = mongoClient.getDatabase("tmsDB");
-		MongoCollection<Document> usersColl = db.getCollection("users");
+			Bson usernameFilter = Filters.eq("username", user.getUsername());
+			Document mongoUser = usersColl.find(usernameFilter).first();
+			if (mongoUser != null) {
+				throw new UsernameAlreadyInUseException();
+			}
 
-		Bson usernameFilter = Filters.eq("username", user.getUsername());
-		Document mongoUser = usersColl.find(usernameFilter).first();
-		if (mongoUser != null) {
-			mongoClient.close();
-			throw new UsernameAlreadyInUseException();
-		}
-		
-		/* Bson emailFilter = Filters.eq("email", user.getEmail());
-		 * Document mongoUser = usersColl.find(emailFilter).first();
+			Bson emailFilter = Filters.eq("email", user.getEmail());
+			mongoUser = usersColl.find(emailFilter).first();
 			if (mongoUser != null) {
 				throw new EmailAlreadyInUseException();
-			} 
-		*/
-		usersColl.insertOne(UserUtils.getMongoUser(user));
+			}
 
-		mongoClient.close();
+			usersColl.insertOne(UserUtils.getMongoUser(user));
+		} finally {
+			mongoClient.close();
+		}
 	}
 }
